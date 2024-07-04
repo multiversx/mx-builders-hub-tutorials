1c076c0c76dd2176da3fdb50a33c7dc13da37fe4
Andrei <giura.andrei70@gmail.com>
2024-07-04 11:57:54 +0300

    default route

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,5 +2,5 @@ import { Navigate } from "react-router-dom";
 import { routeNames } from "../../routes";
 
 export const Home = () => {
-  return <Navigate to={routeNames.dashboard.name} />;
+  return <Navigate to={routeNames.dashboard.children.extensionProvider.name} />;
 };
